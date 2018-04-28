f9818372ec210ca510f67e56aa30cddebe75bae3
Kristine Nutter <35537107+kqn5168@users.noreply.github.com>
2018-04-27 20:08:46 -0400

    labeling original images

--- wavelet_transform.py
+++ wavelet_transform.py
@@ -13,7 +13,7 @@ import pywt
 # Getting the path where the original images are located
 TEST_IMAGE_PATHS = glob.glob("./object_detection/*.jpg")
 
-newpath = './wavelet/' 
+newpath = './original/' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
@@ -29,16 +29,16 @@ for image_path in TEST_IMAGE_PATHS:
 	#grabbing the image
 	img = cv2.imread(image_path)
 
-	# turning it to gray scale
-	gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	# # turning it to gray scale
+	# gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
 
-	#applying the wavelet to the image
-	(cA, (cH, cV, cD)) = pywt.wavedec2(gray_image, 'haar', level=1)
+	# #applying the wavelet to the image
+	# (cA, (cH, cV, cD)) = pywt.wavedec2(gray_image, 'haar', level=1)
 
 	#saveing the LL image as a png to a new folder
-	name = './wavelet/' + str(count) + '.png'
+	name = './original/' + str(count) + '.jpg'
 	print ('Creating...' + name);
-	cv2.imwrite(name, cA)
+	cv2.imwrite(name, img)
 
 
 	count = count + 1
