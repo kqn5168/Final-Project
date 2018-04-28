c02938f9c0d165e7d09ca4ae9793de698aa1a0b8
Kristine Nutter <35537107+kqn5168@users.noreply.github.com>
2018-04-27 20:34:35 -0400

    labeling

--- wavelet_transform.py
+++ wavelet_transform.py
@@ -13,10 +13,13 @@ import pywt
 # Getting the path where the original images are located
 TEST_IMAGE_PATHS = glob.glob("./object_detection/*.jpg")
 
-newpath = './original/' 
+newpath = './wavelet/' 
 if not os.path.exists(newpath):
     os.makedirs(newpath)
 
+newpath2 = './original/' 
+if not os.path.exists(newpath2):
+    os.makedirs(newpath2)
 
 count = 0;
 list=[]
@@ -29,20 +32,25 @@ for image_path in TEST_IMAGE_PATHS:
 	#grabbing the image
 	img = cv2.imread(image_path)
 
-	# # turning it to gray scale
-	# gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+	name2 = './original/' + str(count) + '.jpg'
+	print ('Creating...' + name2);
+	cv2.imwrite(name2, img)
 
-	# #applying the wavelet to the image
-	# (cA, (cH, cV, cD)) = pywt.wavedec2(gray_image, 'haar', level=1)
+	# turning it to gray scale
+	gray_image = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+	#applying the wavelet to the image
+	(cA, (cH, cV, cD)) = pywt.wavedec2(gray_image, 'haar', level=1)
 
 	#saveing the LL image as a png to a new folder
-	name = './original/' + str(count) + '.jpg'
+	name = './wavelet/' + str(count) + '.png'
 	print ('Creating...' + name);
-	cv2.imwrite(name, img)
+	cv2.imwrite(name, cA)
+
+
 
 
 	count = count + 1
-
 
 # When everything done, release the capture
 cv2.destroyAllWindows()
